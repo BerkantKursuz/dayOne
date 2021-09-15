ce9e5f5d3568e9ea67df84e61cca363507dae248
BerkantKursuz <berkantkursuz@gmail.com>
2021-09-15 13:50:20 +0300

    Database bağlantısı Ve Migrationlar
    Database bağlandı. Model oluşturuldu. Giriş sayfası oluşturuldu.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<div style="text-align: center">
+    <h1>Login Page</h1>
+    <form action="" method="post">
+        @csrf
+        Kullanıcı adı : <input type="text" name="username">
+        <br><br>
+        Şifre : <input type="password" name="password">
+        <br><br>
+        <input type="submit" value="Giriş Yap">
+    </form>
+</div>
+</body>
+</html>
